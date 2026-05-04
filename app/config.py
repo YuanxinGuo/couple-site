62f4624348c3970dbd640f9d7308515a666791ce
ggguoyuanxin <ggguoyuanxin@163.com>
2026-05-04 13:34:21 +0800

    修复 pydantic-settings 环境变量读取

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Settings:
         self.secret_key = os.environ.get("SECRET_KEY", "dev-secret-change-me")
         self.site_password = os.environ.get("SITE_PASSWORD", "5201314")
         self.relationship_start_date = date.fromisoformat(
-            os.environ.get("RELATIONSHIP_START_DATE", "2023-05-20")
+            os.environ.get("RELATIONSHIP_START_DATE", "2026-03-01")
         )
         self.database_url = os.environ.get("DATABASE_URL", "sqlite:///./data/couple.db")
         self.cloudinary_cloud_name = os.environ.get("CLOUDINARY_CLOUD_NAME", "")
